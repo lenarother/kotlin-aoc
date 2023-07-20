c385fde137fee78971df7da7f17f414d8b542ff2
Magdalena Rother <magdalena.rother@thermondo.de>
2023-07-20 09:57:39 +0200

    Update dependancies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.9.0"
 }
 
 repositories {
@@ -14,6 +14,6 @@ tasks {
     }
 
     wrapper {
-        gradleVersion = "7.6"
+        gradleVersion = "8.2.1"
     }
 }
