2a039fade86f505f5e57240d23b0042b91a8b9f8
Magdalena Rother <magdalena.rother@thermondo.de>
2023-11-19 15:36:15 +0100

    Part two

--- src/year_2021/day03/Solution.kt
+++ src/year_2021/day03/Solution.kt
@@ -28,8 +28,6 @@ fun getOnesSum(input: List<String>): IntArray {
 
 fun getMostCommonBit(onesCount: Int, allItemsCount: Int): String {
     val allItemsCountHalf = (allItemsCount / 2) + (allItemsCount % 2)
-    println(onesCount)
-    println(allItemsCountHalf)
     return when {
         onesCount < allItemsCountHalf -> "0"
         else -> "1"
@@ -58,8 +56,82 @@ fun part1(input: List<String>): Int {
     return resultNumber.toInt(2) * flippedResultNumber.toInt(2)
 }
 
+/**
+fun filterArray(
+    inputArray: List<String>,
+    countArray: IntArray,
+    n: Int,
+    inputInitialSize: Int,
+    getBitFun: (onesCount: Int, allItemsCount: Int) -> String
+): MutableList<String> {
+    var results:MutableList<String> = mutableListOf()
+    val wantedValue = getBitFun(countArray[n], inputInitialSize)
+    for (i in inputArray) if (i[n].toString() == wantedValue) { results.add(i) }
+    return results
+}
+*/
+
+fun filterArrayMostCommonBit(
+    inputArray: List<String>,
+    countArray: IntArray,
+    n: Int,
+    inputInitialSize: Int,
+): MutableList<String> {
+    var results:MutableList<String> = mutableListOf()
+    val wantedValue = getMostCommonBit(countArray[n], inputInitialSize)
+    for (i in inputArray) if (i[n].toString() == wantedValue) { results.add(i) }
+    return results
+}
+
+fun filterArrayLessCommonBit(
+    inputArray: List<String>,
+    countArray: IntArray,
+    n: Int,
+    inputInitialSize: Int,
+): MutableList<String> {
+    println(inputArray)
+    var results:MutableList<String> = mutableListOf()
+    val wantedValue = getLessCommonBit(countArray[n], inputInitialSize)
+    for (i in inputArray) if (i[n].toString() == wantedValue) { results.add(i) }
+    return results
+}
+
+
+fun findNum1(
+    input: List<String>,
+    countArray: IntArray,
+    inputInitialSize: Int,
+): String {
+    val numberLength = input[0].length - 1
+    for ( i in 0..numberLength ) {
+        var input = filterArrayMostCommonBit(input, countArray, i, inputInitialSize)
+        if (input.size == 1) { return input[0] }
+    }
+    return ""
+}
+
+fun findNum2(
+    input: List<String>,
+    countArray: IntArray,
+    inputInitialSize: Int
+): String {
+    val numberLength = input[0].length - 1
+    for (i in 0..numberLength) {
+        var input = filterArrayLessCommonBit(input, countArray, i, inputInitialSize)
+        if (input.size == 1) {
+            return input[0]
+        }
+    }
+    return ""
+}
+
 fun part2(input: List<String>): Int {
-    return 2
+    val itemsCount = input.size
+    var countArray = getOnesSum(input)
+    println("Count $itemsCount, array $countArray")
+    val firstNum: String = findNum1(input, countArray, itemsCount)
+    val secondNum: String = findNum2(input, countArray, itemsCount)
+    return firstNum.toInt(2) * secondNum.toInt(2)
 }
 
 fun main() {
@@ -67,13 +139,13 @@ fun main() {
     val realInput = readInput("year_2021/day03/resources/RealData")
 
     // Part 1 - Test
-    check(part1(testInput) == 198)
+    //check(part1(testInput) == 198)
 
     // Part 1 - Solution
-    println(part1(realInput))
+    //println(part1(realInput))
 
     // Part 2 - Test
-    // check(part2(testInput) == 2)
+    check(part2(testInput) == 2)
 
     // Part 2 - Solution
     // println(part2(realInput))
