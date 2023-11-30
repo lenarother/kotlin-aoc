68373606980d497c09349bf8a4a57810949498cc
Magdalena Rother <magdalena.rother@thermondo.de>
2023-11-30 14:12:28 +0100

    Day 2 2022 part 2 version with another map

--- src/year_2022/day02/Solution.kt
+++ src/year_2022/day02/Solution.kt
@@ -5,58 +5,86 @@ import readInput
 /**
  * https://adventofcode.com/2022/day/2
  */
+val shapeMap = mapOf(
+    "X" to "Rock",
+    "Y" to "Paper",
+    "Z" to "Scissors",
+    "A" to "Rock",
+    "B" to "Paper",
+    "C" to "Scissors"
+)
 
-fun part1(input: List<String>): Int {
-    println("input: $input")
-    val shapeMap = mapOf(
+val scoreMap = mapOf(
+    "Rock" to 1,
+    "Paper" to 2,
+    "Scissors" to 3,
+)
+
+val scoresMap = mapOf(
+    "Rock" to mapOf(
+        "Rock" to 3,
+        "Paper" to 0,
+        "Scissors" to 6,
+    ),
+    "Scissors" to mapOf(
+        "Rock" to 0,
+        "Paper" to 6,
+        "Scissors" to 3,
+    ),
+    "Paper" to mapOf(
+        "Rock" to 6,
+        "Paper" to 3,
+        "Scissors" to 0,
+    ),
+)
+
+val myShapeMap = mapOf(
+    // Oponent to result
+    "Rock" to mapOf(
+        "X" to "Scissors", // lose
+        "Y" to "Rock", //
+        "Z" to "Paper",
+    ),
+    "Scissors" to mapOf(
+        "X" to "Paper",
+        "Y" to "Scissors",
+        "Z" to "Rock",
+    ),
+    "Paper" to mapOf(
         "X" to "Rock",
         "Y" to "Paper",
         "Z" to "Scissors",
-        "A" to "Rock",
-        "B" to "Paper",
-        "C" to "Scissors"
     )
-    val scoreMap = mapOf(
-        "Rock" to 1,
-        "Paper" to 2,
-        "Scissors" to 3,
-    )
-    println("scoreMap: $scoreMap")
+)
 
-    val scoresMap = mapOf(
-        "Rock" to mapOf(
-            "Rock" to 3,
-            "Paper" to 0,
-            "Scissors" to 6,
-        ),
-        "Scissors" to mapOf(
-            "Rock" to 0,
-            "Paper" to 6,
-            "Scissors" to 3,
-        ),
-        "Paper" to mapOf(
-            "Rock" to 6,
-            "Paper" to 3,
-            "Scissors" to 0,
-        ),
-    )
-    println("scoresMap $scoresMap")
-
-    val sum = input.sumOf { row ->
-        val values = row.split(" ")
-        val opponentShape: String? = shapeMap[values[0]] // a -> rock
-        val myShape: String? = shapeMap[values[1]] // y -paper
-        val winScore = scoresMap[myShape!!]!![opponentShape]!!
-        val roundScore = scoreMap[myShape]!! + winScore
-        println("roundScore $roundScore")
-        roundScore
-    }
+fun part1(input: List<String>): Int {
+        val sum = input.sumOf { row ->
+            val values = row.split(" ")
+            val opponentShape: String? = shapeMap[values[0]] // a -> rock
+            val myShape: String? = shapeMap[values[1]] // y -paper
+            val winScore = scoresMap[myShape!!]!![opponentShape]!!
+            val roundScore = scoreMap[myShape]!! + winScore
+            println("roundScore $roundScore")
+            roundScore
+        }
     return sum
 }
 
-fun part2(input: List<String>): Int {
-    return 2
-}
+
+fun part2MR(input: List<String>): Int {
+        val sum = input.sumOf { row ->
+            val values = row.split(" ")
+            val opponentShape: String? = shapeMap[values[0]] // a -> rock
+            val myShape: String? = myShapeMap[opponentShape!!]!![values[1]]!! // y -paper
+            println("My: $myShape, Op: $opponentShape, R: ${values[1]}" )
+            val winScore = scoresMap[myShape!!]!![opponentShape]!!
+            val roundScore = scoreMap[myShape]!! + winScore
+            // println("roundScore $roundScore")
+            roundScore
+        }
+        return sum
+    }
+
 
 fun main() {
     val testInput = readInput("year_2022/day02/resources/TestData01")
@@ -66,11 +94,11 @@ fun main() {
     check(part1(testInput) == 15)
 
     // Part 1 - Solution
-     println(part1(realInput))
+    println(part1(realInput))
 
     // Part 2 - Test
-    // check(part2(testInput) == 12)
+    check(part2MR(testInput) == 12)
 
     // Part 2 - Solution
-//     println(part2(realInput))
+    println(part2MR(realInput))
 }
